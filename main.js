adb54b1c2a6c3c7377ca6960f23a44ca5b0eb2ed
USIS051620 <marvinjosuesolorzano@gmail.com>
2022-02-20 18:29:01 -0600

    Creacion de componentes en vue.js

--- main.js
+++ main.js
@@ -1,88 +1,23 @@
-var idUnicoFecha = ()=>{
+var generarIdUnicoFecha = ()=>{
     let fecha = new Date();
-    return Math.floor(fecha.getTime()/100).toString(16);
+    return Math.floor(fecha.getTime()/1000).toString(16);
 };
-var app = new Vue({
-    el: '#appCliente',
+var appSistema = new Vue({
+    el: '#appSistema',
     data: {
-        clientes: [],
-        buscar: '',
-        cliente: {
-            accion: 'nuevo',
-            msg : '',
-            idCliente: '',
-            codigo: '',
-            nombre: '',
-            direccion: '',
-            telefono: '',
-            dui: ''
-        },
-    },
-    methods: {
-        buscarCliente(){
-
-            this.obtenerClientes(this.buscar);
-        },
-        guardarCliente(){
-
-            if(this.cliente.accion == 'nuevo'){  
-                this.cliente.idCliente = idUnicoFecha();
-
-
-        }
-        localStorage.setItem(this.cliente.idCliente, JSON.stringify(this.cliente));
-        this.cliente.msg = 'Cliente procesado con exito';
-        this.nuevoCliente();
-        this.obtenerClientes();
-        },
-        modificarCliente(data){
-            this.cliente = data;
-            this.cliente.accion = 'modificar';
-        },
-        eliminarCliente(data){
-            if( confirm(`¿Esta seguro de eliminar el cliente ${data.nombre}?`) ){
-                this.cliente.idCliente = data.idCliente;
-                this.cliente.accion = 'eliminar';
-                this.guardarCliente();
-                localStorage.removeItem(cliente.idCliente);
-                this.obtenerClientes();
-            }
-            this.nuevoCliente();
-        },
-        obtenerClientes(valor=''){
-            this.clientes = [];
-                    for(let i=0; i<localStorage.length; i++){
-                        let key = localStorage.key(i);
-                        if( valor.trim().length>0 ){
-                            let cliente = JSON.parse(localStorage.getItem(key));
-                            if( cliente.nombre.toLowerCase().indexOf(valor.toLowerCase()) > -1 ){
-                                this.clientes.push(cliente);
-
-            /*this.clientes = [];
-            for(let i=0; i<results.rows.length; i++){
-                 this.clientes.push(results.rows.item(i));
-           }*/
-        } 
-          }else {
-            this.clientes.push(JSON.parse(localStorage.getItem(key)));
-        }
-        }
-
-        },
-        nuevoCliente(){
-            this.cliente.accion = 'nuevo';
-            this.cliente.idCliente = '';
-            this.cliente.codigo = '';
-            this.cliente.nombre = '';
-            this.cliente.direccion = '';
-            this.cliente.telefono = '';
-            this.cliente.dui = '';
-            this.cliente.msg = '';
-            console.log(this.cliente);
-            
+        forms:{
+            'cliente':{mostrar:false},
+            'producto':{mostrar:false},
+            'proveedor':{mostrar:false},
+            'categoria':{mostrar:false}
         }
     },
-    created(){
-        this.obtenerClientes();
-    }
+});
+document.addEventListener('DOMContentLoaded', e=>{
+    let formularios = document.querySelectorAll('.mostrar').forEach(formulario=>{
+        formulario.addEventListener('click', evento=>{
+            let formulario = evento.target.dataset.form;
+            appSistema.forms[formulario].mostrar = true;
+        });
+    });
 });
